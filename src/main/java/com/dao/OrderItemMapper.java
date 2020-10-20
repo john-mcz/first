06a66c3d9bc734bee28768c7ffd92821cdcfeeac
john-mcz <26889307@qq.com>
2020-10-20 14:20:36 +0800

    #

---
+++ src/main/java/com/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.dao;
+
+import com.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
